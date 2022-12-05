94bddc39fac0c77258e7771b05ca90e4470889d1
vishnu <vishnu@NTP-LAP-767.chn.nestgroup.net>
2022-12-05 21:31:39 +0530

    initial

---
+++ src/test/java/com/nest/employee_backend/EmployeeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nest.employee_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
